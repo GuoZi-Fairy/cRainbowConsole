68ad46e1affbd64fc87fb4f0d992875319b75f61
GuoZi-Fairy <5710385+bupt_xyg@user.noreply.gitee.com>
2020-02-19 22:01:57 +0800

    parser done

--- ccRainbow.c
+++ ccRainbow.c
@@ -1,129 +1,228 @@
-//TODO:json-parser:
+//TODO:parser:
 /*
-"[{"text":"","color":"red"},{"text":"world","color":"blue"}]"
-"{\"text\":\" \",\"color\":\"red\"}"
-"\"text\":\"hello\",\"color\":\"red\""
-"\"text\":\"hello\""
-"hello"
-%d %c %s
-可将上述类型解析为
-text:hello world
-color:red
-实现:
-    解析json，并建立数据类型
+"{red}str..%d.."
+rule:
+若读入'{' 弹出栈中所有元素解析并输出
+若读入'\' 则检查是否为'\{'或'\}' 若是则不触发栈操作
+若读入'\0' 则弹出栈中所有元素解析并输出并结束
+PS:在没有读入'}'时不能弹出'{'
+若解析'{' 一直读入字符至parser_color中 直到读入'}'
+若解析'}' 则直接将后方所有元素抛入输出中
+若解析到'%d/%c/%s/%f/...',则将参数以相同参数输出
+若解析到'\}'或'\{' 则先替换为{/}再抛入输出中
 */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 #define STDCAL(type) type __cdecl
-
-typedef struct rainbowItem rItem;//"key":"value"
-typedef struct rainbowJson rJson;//rItem
+/*************************************************************/
+/*************************************************************/
 
 typedef struct error
 {
     char* error_type;
     char* error_msg;
-    unsigned int position;//为错误解析的索引
 }error;
-
+typedef enum rainbow_color
+{
+    white,
+    red,
+    clear
+}rColor;
 //__bool
-typedef enum ccRanbow_boolea
+typedef enum ccRainbow_boolea
 {
     False,
     True
 }boolean;
+
 #define RAINBOW_PARSER const char*
+
 typedef struct ranbowcontext
 {
-    RAINBOW_PARSER json;//表示当前指向的字符
-    size_t index;
+    RAINBOW_PARSER context;//表示当前指向的字符
+    char* stack;
+    size_t size,top;
 }rContext;
-typedef struct ranbowpos //会对解析字符串的大小产生限制
-{
-    size_t start;//为第一个"在字符串中的索引
-    size_t end;//为第二个"在字符串的索引
-}rPos;
-
 typedef struct rainbowItem
 {
-    char* key;
-    char* value;//rainbowJson could only use string to be value
-    rItem* next;//to the next rItem
+    char* output;
+    rColor color;
 }rItem;
-typedef struct rainbowJson
-{
-    rItem* node;//to the fist Item of Json_obj
-    rJson* next;
-}rJson;
+static STDCAL(void*) rainbow_stack_push(rContext* parser,size_t size);
+static STDCAL(void*) rainbow_stack_pop(rContext* parser,size_t size);
+static STDCAL(void) rainbow_set_output(rContext* parser,rItem* item,char* stack,size_t size);
+static STDCAL(rColor) rainbow_parser_color(char* color);
+static STDCAL(void) rainbow_parse_stack(rContext* parser,rItem* item);
+static STDCAL(void) rainbow_parse(const char* text);
+static STDCAL(void) rainbow_count_token(rItem* item);
+static STDCAL(void) rainbow_output(rItem* item);
 /********************************************************/
-static STDCAL(void) rainbow_parse_whitespace(rContext* parser)
+error color_end_token_error = {"color_parse_error","please check '}' at the end of string"};
+error Invild_color_error = {"Invild color value","please check your color value"};
+#define RAISE(ERROR) do{printf("ERROR:\n[%s]:%s\n",ERROR.error_type,ERROR.error_msg);system("pause");system("exit");}while(0)
+/********************************************************/
+static STDCAL(void) rainbow_context_init(rContext* parser,const char* text)
 {
-    RAINBOW_PARSER p = parser->json;
-    while (*p=='\t'||*p==' '||*p=='\n'||*p=='\r')
+    parser->context = text;
+    parser->stack = NULL;
+    parser->size = 0;
+    parser->top = 0;
+}
+/********************************************************/
+#ifndef INIT_STACK_SIZE
+#define INIT_STACK_SIZE 256
+#endif
+static STDCAL(void) forech_stack(rContext* parser)
+{
+    for (size_t i = 0; i < parser->top; i++)
     {
-        p++;
-        (parser->index)++;
+        printf("%lld:%c\n",i,parser->stack[i]);
     }
-    parser->json = p;
+    
 }
-/*******************************************************/
-static STDCAL(void) rainbow_set_string(rItem* item,rPos* pos,const char* text)
+static STDCAL(void*) rainbow_stack_push(rContext* parser,size_t size)
 {
-    size_t len = (pos->end)-(pos->start)+1;//被获取的字符串的实际长度
-    free(item->value);
-    item->value = (char*)malloc(sizeof(char)*(len+1));
-    char* p_item = item->value;
-    const char* p_text = text + (pos->start)+1;//字符串中第一个字符 除去\"
-    for(size_t i = (pos->start+1); i< (pos->end); i++, p_item++ , p_text++) *p_item = *p_text;
-    *(p_item) = '\0';//set the end to \0
-}
-static STDCAL(void) rainbow_parse_string(rContext* parser)
-{
-}
-/********************************************************/
-static STDCAL(void) rContext_init(rContext* parser,const char* text)
-{
-    parser->json  = text;
-    parser->index = 0;
-}
-/********************************************************/
-static STDCAL(int) rainbow_parse(const char* text)
-{
-    rContext parser;
-    rContext_init(&parser,text);
-    rainbow_parse_whitespace(&parser);
-    switch (*(parser.json))
+    void* ret;//为值的位置
+    assert(size>0);
+    if ((parser->size)-(parser->top) <= size)//栈大小不足
     {
-    case '\"':
-        rainbow_parse_string(&parser);
-        break;
-    case '\0':
-        return 0;
-    default:
-        break;
+        if (parser->size == 0) parser->size=INIT_STACK_SIZE;
+        while (parser->top+size>=parser->size)parser->size += parser->size>>1;
+        parser->stack = (char*)realloc(parser->stack,parser->size);
     }
+    ret = parser->stack+parser->top;
+    parser->top+= size;
+    return ret;
+}
+#define PUSH(PARSER,CH) do{*(char*)rainbow_stack_push(PARSER,sizeof(CH))=(CH);}while(0) //用于调用压栈操作
+static STDCAL(void*) rainbow_stack_pop(rContext* parser,size_t size)
+{
+    void* ret;
+    assert(parser->top >= size);
+    parser->top -= size;
+    ret = parser->stack+parser->top;
+    return ret;
+}
+/********************************************************/
+static STDCAL(void) rainbow_set_output(rContext* parser,rItem* item,char* stack,size_t size)//size为栈中需要拷贝的大小，stack为栈中需要拷贝的起始位置
+{
+    assert(parser->stack!=NULL);
+    item->output = (char*)malloc(size+1);
+    memcpy(item->output,stack,size);
+    *(item->output+size)='\0';
+}
+static STDCAL(rColor) rainbow_parser_color(char* color)
+{
+    if(!strcmp(color,"red"))return red;
+    else if(!strcmp(color,"clear"))return clear;
+    else if(!strcmp(color,"white"))return white;
+    else RAISE(Invild_color_error);
+    return clear;
+}
+static STDCAL(void) rainbow_parse_stack(rContext* parser,rItem* item)
+{
+    assert(parser->size!=0);
+    char* c = parser->stack;
+    size_t size = 0;
+    if(*c=='{')
+    {
+        char color[20];
+        int i;
+        c++;
+        for(i = 0;(*c=='}' && *(c-1)=='\\' && i<20) || (*c!='}' && i<20);c++,i++)
+        {
+            if (size>=parser->size)RAISE(color_end_token_error);
+            color[i] = *c;
+            size += sizeof(*c);
+        }
+        size += 2;//添加'{'和'}'所占的内存
+        color[i] = '\0';
+        item->color = rainbow_parser_color(color);
+        c++;
+    }
+    else item->color = clear;
+    rainbow_set_output(parser,item,c,parser->top-size);
+}
+#define PUSH_THIS() PUSH(&rcontext,*rcontext.context);
+// #define PUSH_THIS() do{PUSH(&rcontext,*rcontext.context);printf(">\'%c\' pushed\n",*rcontext.context);}while(0)
+static STDCAL(void) rainbow_parse(const char* text)
+{
+    rContext rcontext;
+    rainbow_context_init(&rcontext,text);
+    while (*rcontext.context!='\0')
+    {
+        switch (*rcontext.context)
+        {
+            case '{':
+                {
+                    rItem item;
+                if(rcontext.size!=0)
+                {
+                    rainbow_parse_stack(&rcontext,&item);
+                    rainbow_output(&item);
+                    rcontext.stack = (char*)rainbow_stack_pop(&rcontext,rcontext.top);
+                    PUSH_THIS();
+                }
+                else PUSH_THIS();
+                break;
+                }
+            case '\\':
+            {
+                if(*(rcontext.context+1)=='{'||*(rcontext.context+1)=='}')
+                {
+                    PUSH_THIS();
+                    rcontext.context++;
+                    PUSH_THIS();
+                }
+                else PUSH_THIS();
+                break;
+            }
+            default:
+            {
+                PUSH_THIS();
+            }
+        }
+        rcontext.context++;
+    }
+    rItem item;
+    rainbow_parse_stack(&rcontext,&item);
+    rainbow_output(&item);
+    rcontext.stack = (char*)rainbow_stack_pop(&rcontext,rcontext.top);
+    free(rcontext.stack);
+    free(item.output);
+}
+
+#undef PUSH_THIS
+/********************************************************/
+static STDCAL(void) rainbow_output(rItem* item)
+{
+    char* ch = item->output;
+    while (ch!='/0')
+    {
+        switch (*ch)
+        {
+            case '%':
+                {
+                    
+                    break;
+                }
+        }
+    }
+    
 }
 /********************************************************/
 #ifdef _WIN32
     //TODO: code for windows os
-    #include <conio.h>
-#endif
-
-#ifdef __APPLE__||__linux
-    //TODO:code for osX/linux
-    #define RED 0x0004
-#endif
-
-//for test
-int main(int argc, char const *argv[])
-{
-    rPos pos = {6,12};
-    const char test[] = "hello \"world\",hello cctv, hello bilibili";
-    rItem test_item;
-    test_item.key = "test";
-    test_item.value = (char*)malloc(sizeof(char)*100);
-    rainbow_set_string(&test_item,&pos,test);
-    printf("::%s::",test_item.value);
     
+#elif __APPLE__||__linux
+    //TODO:code for osX/linux
+#endif
+
+int main(int argc, char const *argv[])
+{   
+    const char* test = "{red}\\{\\}{white}hehe";
+    rainbow_parse(test);
     return 0;
 }
--- test.c
+++ test.c
@@ -2,10 +2,8 @@
 #include "ccRainbow.h"
 int main(int argc, char const *argv[])
 {
-    char test[] = "{\"  text\":\"hello world\",\"red\"}";
-    
-    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | FOREGROUND_RED);
-    printf("hello world");
+    // char test[] = "{\"  text\":\"hello world\",\"red\"}";
+    // HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    // SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | FOREGROUND_RED);
     return 0;
 }
